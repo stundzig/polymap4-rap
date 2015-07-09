3012d45b875e2e7f40d16279fde94f0bc064bc75
Steffen Stundzig <git@stundzig.de>
2015-07-09 21:40:07 +0200

    #9

--- org.polymap.rap.openlayers/src/org/polymap/rap/openlayers/style/Style.java
+++ org.polymap.rap.openlayers/src/org/polymap/rap/openlayers/style/Style.java
@@ -14,7 +14,6 @@
 package org.polymap.rap.openlayers.style;
 
 import org.polymap.core.runtime.config.Concern;
-import org.polymap.core.runtime.config.Mandatory;
 import org.polymap.core.runtime.config.Property2;
 import org.polymap.rap.openlayers.base.OlObject;
 import org.polymap.rap.openlayers.base.OlPropertyConcern;
@@ -24,7 +23,7 @@ import org.polymap.rap.openlayers.base.OlPropertyConcern;
  * its children will not take effect until the feature, layer or FeatureOverlay that
  * uses the style is re-rendered.
  * 
- * @see href="http://openlayers.org/en/master/apidoc/ol.style.Style.html">OpenLayers Doc</a>
+ * @see <a href="http://openlayers.org/en/master/apidoc/ol.style.Style.html">OpenLayers Doc</a>
  * @author <a href="http://www.polymap.de">Falko Bräutigam</a>
  * @author <a href="http://stundzig.it">Steffen Stundzig</a>
  */
